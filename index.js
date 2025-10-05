d44dab099ccd6830eeb5898d6ef471c260f29074
anirban <anirbanguharoy82@gmail.com>
2025-10-06 00:59:21 +0530

    delete route some changes

--- index.js
+++ index.js
@@ -3,6 +3,30 @@ const app=express()
 app.use(express.json())
 
 let todos=[
+  {
+    "title":"1",
+    "description":"1",
+    "isChecked":false,
+    "id":1
+  },
+   {
+    "title":"2",
+    "description":"2",
+    "isChecked":false,
+    "id":2
+  },
+   {
+    "title":"3",
+    "description":"3",
+    "isChecked":true,
+    "id":3
+  },
+   {
+    "title":"4",
+    "description":"4",
+    "isChecked":false,
+    "id":4
+  }
 
 ]
 
@@ -33,16 +57,28 @@ app.get("/todos",(req,res)=>{
 //   res.status(200).json({"message":"kya hal hei vai ke"})
 // })
 
-app.delete("/todos/:id",(req,res)=>{
-  // console.log(req.body)
-  try {
-    // console.log(req.params.id)
+// app.delete("/todos/:id",(req,res)=>{
+//   // console.log(req.body)
+//   try {
+//     // console.log(req.params.id)
     
-    todos.splice(Number(req.params.id) + 1,1)
-    return res.status(200).json({message:"Todo delete successfully"})
+//     todos.splice(Number(req.params.id) + 1,1)
+//     return res.status(200).json({message:"Todo delete successfully"})
+    
+//   } catch (error) {
+//     return res.status(500).json({"message":"Please try again"})
+    
+//   }
+// })
+
+app.delete("/todos/:id",(req,res)=>{
+  try {
+    const id=Number(req.params.id)
+     todos=todos.filter(todo=>todo.id!==id)
+    return res.status(200).json({message:"Todo deleted successfullly"})
     
   } catch (error) {
-    return res.status(500).json({"message":"Please try again"})
+    return res.status(500).json({message:"Please try again"})
     
   }
 })
